8e66ad3852f3c768cafee659c4cf46f60beae8fe
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-08-16 00:54:27 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="border-b p-4">
+        Medium app
+        </div>
+        {children}
+        </body>
     </html>
   );
 }
---
+++ app/signin/layout.tsx
@@ -0,0 +1,9 @@
+
+export default function({children}: {children: React.ReactNode}){
+    return <div>
+        <div className="border-b text-center">
+        20% off for next three days
+    </div>
+    {children}
+    </div>
+}
